d71c8068a73a23792f56945c9011c82c07772850
Marcio Silva <marciosilvadrum@gmail.com>
2022-11-19 12:40:39 -0300

    get the colletcion to home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,6 +4,17 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 function Home() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      const response = await axios.get("https://ironrest.herokuapp.com/stocks");
+      setUsers(response.data);
+      // console.log(response.data);
+    }
+    fetchUsers();
+  }, []);
+
   return (
     <div>
       <center>
