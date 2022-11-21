587481817d42a93dacd533c83f8d3be66aa37bf4
Vitor Martins Costa <costa.vsm@gmail.com>
2022-11-21 14:15:51 -0300

    New components

---
+++ src/components/ChangeOp.js
@@ -0,0 +1,13 @@
+function ChangeOp() {
+    
+    //Achar indice da operação que queremos alterar
+    //Clonar op
+    //Slice Op
+    //Push form novo
+    
+    return (
+        <></>
+    )
+}
+
+export default ChangeOp
