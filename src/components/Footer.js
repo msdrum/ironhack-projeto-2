7ce46a1a938df1524c602fad6bce4c8bc6ad7b3a
Karen <karentomedi@gmail.com>
2022-11-23 16:23:35 -0300

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+
+function Footer() {
+  return (
+    <footer className="footer">
+        <span>Karen Ortiz</span>
+              |      
+        <span>Marcio Silva</span>
+               |       
+        <span>Vitor Martins </span>
+    </footer>
+  );
+}
+
+export default Footer;
