48bccc8c2e1e71e1f42bef7a29a2427284da66db
Karen <karentomedi@gmail.com>
2022-11-23 03:39:53 -0300

    css home

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,18 @@
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
     <Navbar className="navBar">
-      <Container>
         <Link to="/" style={{ textDecoration: "none" }}>
-          <Navbar.Brand as="div">
             <img
               alt="logo da ironhack em branco"
               src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/landing-page/ironhack-logo-xs.png"
               width="30"
               height="30"
-              className="d-inline-block align-top"
             />{" "}
-               Home
-          </Navbar.Brand>
+            <span className="wallet">wallet</span>
         </Link>
-      </Container>
     </Navbar>
   );
 }
--- src/components/NewUser.js
+++ src/components/NewUser.js
@@ -3,25 +3,30 @@ import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-function NewUser({newUser, setNewUser}) {
-    const navigate = useNavigate()
+function NewUser({ newUser, setNewUser }) {
+  const navigate = useNavigate();
 
-    function handleChange(e) {
-        setNewUser(e.target.value)
-      }
-    
-    function handleClick() {
-        navigate(`/${newUser}`)
-      }
-    
-    
-    return (
-        <>
-            <Form.Control type="text" placeholder="Nome da Carteira" onChange={handleChange}/>
-        
-            <button onClick={handleClick}>Create new user</button>
-        </>
-    )
+  function handleChange(e) {
+    setNewUser(e.target.value);
+  }
+
+  function handleClick() {
+    navigate(`/${newUser}`);
+  }
+
+  return (
+    <>
+      <Form.Control
+        type="text"
+        placeholder="Nome da Carteira"
+        onChange={handleChange}
+      />
+
+      <button className="botaoConfirmar" onClick={handleClick}>
+        CONFIRMAR
+      </button>
+    </>
+  );
 }
 
-export default NewUser
+export default NewUser;
