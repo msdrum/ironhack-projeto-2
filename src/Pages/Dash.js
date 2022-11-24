c121f4345c45dcff9b5122af20658da54c8bff8a
Marcio Silva <marciosilvadrum@gmail.com>
2022-11-24 13:35:43 -0300

    changing api address

--- src/Pages/Dash.js
+++ src/Pages/Dash.js
@@ -14,7 +14,7 @@ function Dash() {
     async function fetchPositions() {
       try {
         const response = await axios.get(
-          "http://ironrest.herokuapp.com/minha-carteira"
+          "https://ironrest.herokuapp.com/minha-carteira"
         );
         // console.log(response.data);
         setPositions(response.data);
@@ -26,78 +26,80 @@ function Dash() {
     fetchPositions();
   }, [reload]);
 
-  selectedWallet.sort((a,b) => {return a.ticker<b.ticker ? -1: 1;});
+  selectedWallet.sort((a, b) => {
+    return a.ticker < b.ticker ? -1 : 1;
+  });
 
   return (
     <div className="dash-container">
       <div className="dash-main">
-      <div className="dash-portofolio">
-      <ModalNew walletID={walletID} reload={reload} setReload={setReload} />
+        <div className="dash-portofolio">
+          <ModalNew walletID={walletID} reload={reload} setReload={setReload} />
 
-      {selectedWallet.map((i) => {
-        const quantidadeTotal =
-          i.op
-            .filter((op) => op.tipo === "Compra")
-            .map((op) => +op.qtd)
-            .reduce((a, b) => a + +b, 0) -
-          i.op
-            .filter((op) => op.tipo === "Venda")
-            .map((op) => +op.qtd)
-            .reduce((a, b) => a + +b, 0);
-        const precoMedio =
-          i.op
-            .filter((op) => op.tipo === "Compra")
-            .map((op) => +op.preco * +op.qtd)
-            .reduce((a, b) => a + +b, 0) /
-          i.op
-            .filter((op) => op.tipo === "Compra")
-            .map((op) => +op.qtd)
-            .reduce((a, b) => a + +b, 0);
+          {selectedWallet.map((i) => {
+            const quantidadeTotal =
+              i.op
+                .filter((op) => op.tipo === "Compra")
+                .map((op) => +op.qtd)
+                .reduce((a, b) => a + +b, 0) -
+              i.op
+                .filter((op) => op.tipo === "Venda")
+                .map((op) => +op.qtd)
+                .reduce((a, b) => a + +b, 0);
+            const precoMedio =
+              i.op
+                .filter((op) => op.tipo === "Compra")
+                .map((op) => +op.preco * +op.qtd)
+                .reduce((a, b) => a + +b, 0) /
+              i.op
+                .filter((op) => op.tipo === "Compra")
+                .map((op) => +op.qtd)
+                .reduce((a, b) => a + +b, 0);
 
-        async function calcData() {
-          await axios.put(
-            `https://ironrest.herokuapp.com/minha-carteira/${i._id}`,
-            { pm: precoMedio }
-          );
+            async function calcData() {
+              await axios.put(
+                `https://ironrest.herokuapp.com/minha-carteira/${i._id}`,
+                { pm: precoMedio }
+              );
 
-          await axios.put(
-            `https://ironrest.herokuapp.com/minha-carteira/${i._id}`,
-            { qtd_total: quantidadeTotal }
-          );
-        }
+              await axios.put(
+                `https://ironrest.herokuapp.com/minha-carteira/${i._id}`,
+                { qtd_total: quantidadeTotal }
+              );
+            }
 
-        calcData();
+            calcData();
 
-        return (
-          <div key={i._id}>
-            <Link to={`/${i.carteira}/${i._id}`}>
-              <h4>{i.ticker}</h4>
-            </Link>
-            <p>
-              PM:{" "}
-              {i.pm.toLocaleString("pt-br", {
-                style: "currency",
-                currency: "BRL",
-              })}
-            </p>
-            <p>QTD TOTAL: {i.qtd_total}</p>
-            <p>
-              VALOR TOTAL:{" "}
-              {(i.qtd_total * i.pm).toLocaleString("pt-br", {
-                style: "currency",
-                currency: "BRL",
-              })}
-            </p>
-          </div>
-        );
-      })}
-      </div>
-      <Routes>
-        <Route
-          path=":stockID"
-          element={<StockDetail selectedWallet={selectedWallet} />}
-        />
-      </Routes>
+            return (
+              <div key={i._id}>
+                <Link to={`/${i.carteira}/${i._id}`}>
+                  <h4>{i.ticker}</h4>
+                </Link>
+                <p>
+                  PM:{" "}
+                  {i.pm.toLocaleString("pt-br", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
+                </p>
+                <p>QTD TOTAL: {i.qtd_total}</p>
+                <p>
+                  VALOR TOTAL:{" "}
+                  {(i.qtd_total * i.pm).toLocaleString("pt-br", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+        <Routes>
+          <Route
+            path=":stockID"
+            element={<StockDetail selectedWallet={selectedWallet} />}
+          />
+        </Routes>
       </div>
     </div>
   );
