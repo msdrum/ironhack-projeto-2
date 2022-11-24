cadcb39334fa11a8546327bcb9b1dde3b7b2ed7d
Karen <karentomedi@gmail.com>
2022-11-24 14:10:28 -0300

    teste

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,8 @@
-
 function Footer() {
   return (
     <footer className="footer">
-        <span>Karen Ortiz</span>
-              |      
-        <span>Marcio Silva</span>
-               |       
-        <span>Vitor Martins </span>
+      <span>Karen Louise Ortiz</span>|<span>Marcio Silva</span>|
+      <span>Vitor Martins </span>
     </footer>
   );
 }
